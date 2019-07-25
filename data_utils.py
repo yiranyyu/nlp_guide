4082cdf3d93ffaf2b2a5e669f2449e54ed65f71d
yiranyyu <2606375857@qq.com>
2019-07-25 13:00:28 +0800

    delete not used file

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,47 @@
 import torch
 import os
-from reader import ptb_raw_data
+import collections
+import os
+
+
+def read_words(filename):
+    with open(filename, "r") as f:
+        return f.read().replace("\n", "<eos>").split()
+
+
+def word2id(filename):
+    words = read_words(filename)
+    counter = collections.Counter(words)
+
+    # sort with decreasing frequency, if frequency is same, sort by the word in lexicon order
+    # result is in format like [word_1: freq_1,
+    #                           word_2: freq_2,
+    #                           ...,
+    #                           word_n: freq_n]
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+
+    return word_to_id
+
+
+def file_to_word_ids(filename, word_to_id):
+    data = read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+
+def ptb_raw_data(data_path=None):
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
+
+    word_to_id = word2id(train_path)
+    train_data = file_to_word_ids(train_path, word_to_id)
+    valid_data = file_to_word_ids(valid_path, word_to_id)
+    test_data = file_to_word_ids(test_path, word_to_id)
+    vocab_size = len(word_to_id)
+    return train_data, valid_data, test_data, vocab_size
 
 
 class Dictionary(object):
--- main_lm.py
+++ main_lm.py
@@ -2,24 +2,29 @@ import sys
 import os
 
 if __name__ == "__main__":
-    hidden_sizes = [1500]
-    lrs = [0.0001]
-    max_epoch = 200
-    seeds = [233, 777, 42]
     verbose = 0
+    max_epoch = 200
 
-    data_dir = './data/language_model/'
+    lrs = [0.0001]
+    # seeds = [233, 777, 42]
+    seeds = [233]
+    dropouts = [0.5]
+    hidden_sizes = [400]
+
+    data_dir = './data/language_model/sample'
     model_dir = './model'
 
     for hidden_size in hidden_sizes:
         for lr in lrs:
-            for seed in seeds:
-                cmd = ' '.join(['python', './train_torch_lm.py',
-                                '-data_dir ', data_dir,
-                                '-model_dir', model_dir,
-                                '-hidden_size', str(hidden_size),
-                                '-max_epoch', str(max_epoch),
-                                '-seed', str(seed),
-                                '-verbose', str(verbose),
-                                '-lr', str(lr)])
-                os.system(cmd)
+            for dropout in dropouts:
+                for seed in seeds:
+                    cmd = ' '.join(['python', './train_torch_lm.py',
+                                    '-data_dir ', data_dir,
+                                    '-model_dir', model_dir,
+                                    '-hidden_size', str(hidden_size),
+                                    '-max_epoch', str(max_epoch),
+                                    '-seed', str(seed),
+                                    '-verbose', str(verbose),
+                                    '-dropout', str(dropout),
+                                    '-lr', str(lr)])
+                    os.system(cmd)
